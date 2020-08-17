4b75d819addb5acb117b0d32d33251d2332d33b0
Stefanie <stefcodes@gmail.com>
2020-08-16 17:03:45 -0700

    add form input component and formatting

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './sign-in.styles.scss'; 
 
+import FormInput from '../form-input/form-input.component';
+
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -33,21 +35,22 @@ handleChange = event => {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput
                         name="email" 
                         type="email" 
-                        onChange={this.handleChange} 
-                        value={this.state.email} 
-                        required />
-                    <label>Email</label>
-                    <input 
+                        handleChange={this.handleChange} 
+                        value={this.state.email}
+                        label="email"
+                        required 
+                    />
+                    <FormInput
                         name="password" 
                         type="password" 
                         value={this.state.password} 
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label="password"
                         required 
                     />
-                    <label>Password</label>
 
                     <input type="submit" value="Submit Form"/>
                 </form>
